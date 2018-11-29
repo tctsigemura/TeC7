f805d68c8a7e34c1fadea92a8a8a20abcea7618b
i14kwmr <45026126+i14kwmr@users.noreply.github.com>
2018-11-29 14:17:20 +0900

    transform tab to space
    tabからスペースに置き直しを行なった

--- VHDL/TaC/tac_mmu.vhd
+++ VHDL/TaC/tac_mmu.vhd
@@ -30,18 +30,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity tac_mmu is
-    Port ( P_CLK : in  STD_LOGIC;
-           P_RESET : in  STD_LOGIC;
-           P_EN : in  STD_LOGIC;
-           P_IOR : in  STD_LOGIC;
-           P_IOW : in  STD_LOGIC;
-           P_INT : out  STD_LOGIC;
-           P_ADDR : in  STD_LOGIC;                              -- 仮想アドレス
+    Port (
+	   P_CLK : in  STD_LOGIC;
+	   P_RESET : in  STD_LOGIC;
+	   P_RESET : in  STD_LOGIC;
+	   P_EN : in  STD_LOGIC;
+	   P_IOR : in  STD_LOGIC;
+	   P_IOW : in  STD_LOGIC;
+	   P_INT : out  STD_LOGIC;
+	   P_ADDR : in  STD_LOGIC;                              -- 仮想アドレス
 	   P_MMU_ADDR : in  STD_LOGIC_VECTOR (15 downto 0);     -- I/O用アドレス
-           P_DIN : in  STD_LOGIC_VECTOR (15 downto 0);          -- B,Lの値
-           P_DOUT : out  STD_LOGIC_VECTOR (15 downto 0);        -- 物理アドレス
-           P_B : out  STD_LOGIC_VECTOR (15 downto 0);           -- Bレジスタへの出力
-           P_L : out  STD_LOGIC_VECTOR (15 downto 0));          -- Lレジスタへの出力
+	   P_DIN : in  STD_LOGIC_VECTOR (15 downto 0);          -- B,Lの値
+	   P_DOUT : out  STD_LOGIC_VECTOR (15 downto 0);        -- 物理アドレス
+	   P_B : out  STD_LOGIC_VECTOR (15 downto 0);           -- Bレジスタへの出力
+	   P_L : out  STD_LOGIC_VECTOR (15 downto 0));          -- Lレジスタへの出力
 end tac_mmu;
 
 architecture Behavioral of tac_mmu is
@@ -49,25 +51,25 @@ architecture Behavioral of tac_mmu is
 	signal i_l : STD_LOGIC_VECTOR (15 downto 0);
 
 begin
-	process(P_RESET, P_CLK)
-		begin
-			if (P_RESET='0') then
-				i_b <= "0000000000000000";
-				i_l <= "0000000000000000";
-			elsif (P_CLK'event and P_CLK='1') then
-				if (P_EN='1' and P_IOW='1') then
-					if (P_ADDR='0') then
-						i_b <= P_DIN;
-					else
-						i_l <= P_DIN;
-					end if;
-				end if;
-			end if;
-		end process;
+  process(P_RESET, P_CLK)
+  begin
+    if (P_RESET='0') then
+      i_b <= "0000000000000000";
+      i_l <= "0000000000000000";
+    elsif (P_CLK'event and P_CLK='1') then
+      if (P_EN='1' and P_IOW='1') then
+        if (P_ADDR='0') then
+          i_b <= P_DIN;
+        else
+          i_l <= P_DIN;
+        end if;
+      end if;
+    end if;
+  end process;
 
-	P_DOUT <= "0000000000000000"; -- 今のところ未設定
-	P_B <= i_b;
-	P_L <= i_l;
-	P_INT <= '0'; -- 今のところ未設定
+  P_DOUT <= "0000000000000000"; -- 今のところ未設定
+  P_B <= i_b;
+  P_L <= i_l;
+  P_INT <= '0'; -- 今のところ未設定
 
 end Behavioral;
