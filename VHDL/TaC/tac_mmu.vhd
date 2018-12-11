ccf2685e13eeb1c30c7fca3d211f21c4f1416c91
i14kwmr <i14kawamura@gmail.com>
2018-12-11 14:09:00 +0900

    MOD VHD tac.vhd,tac_mmu.vhd

--- VHDL/TaC/tac_mmu.vhd
+++ VHDL/TaC/tac_mmu.vhd
@@ -6,28 +6,28 @@ use ieee.std_logic_unsigned.all;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity tac_mmu is
+entity TAC_MMU is
   Port (
     P_CLK : in  STD_LOGIC;
     P_RESET : in  STD_LOGIC;
     P_EN : in  STD_LOGIC;
     P_IOW : in  STD_LOGIC;
     P_MMU_MR : in  STD_LOGIC;
-    P_EXE_MODE : in  STD_LOGIC;                          -- Execution mode (0:user, 1:privilege)
+    P_PR : in  STD_LOGIC;                          -- Execution mode (0:user, 1:privilege)
     P_INT : out  STD_LOGIC;
     P_MR : out  STD_LOGIC;
     P_ADDR : out  STD_LOGIC_VECTOR (15 downto 0);        -- Physical address
     P_MMU_ADDR : in  STD_LOGIC_VECTOR (15 downto 0);     -- Virtual address, P_MMU_ADDR(1):I/O address
     P_DIN : in  STD_LOGIC_VECTOR (15 downto 0)           -- B,L register (input)
   );
-end tac_mmu;
+end TAC_MMU;
 
-architecture Behavioral of tac_mmu is
-signal i_mr : STD_LOGIC ;
+architecture Behavioral of TAC_MMU is
+signal i_mr   : STD_LOGIC ;
 signal i_intr : STD_LOGIC ;
-signal i_b : STD_LOGIC_VECTOR (15 downto 0);             -- B register
-signal i_l : STD_LOGIC_VECTOR (15 downto 0);             -- L register
-signal i_addr : STD_LOGIC_VECTOR (15 downto 0);          -- for Physical address　calculation
+signal i_b    : STD_LOGIC_VECTOR (15 downto 0);    -- B register
+signal i_l    : STD_LOGIC_VECTOR (15 downto 0);    -- L register
+signal i_addr : STD_LOGIC_VECTOR (15 downto 0);    -- for Physical address　calculation
 
 begin
   process(P_RESET, P_CLK)
@@ -46,9 +46,9 @@ begin
     end if;
   end process;
   
-  i_addr <= (P_MMU_ADDR + i_b) when (P_EXE_MODE = '1')
-            else P_MMU_ADDR ;                                   -- addition func
-  i_intr <= '1' when ((P_MMU_ADDR >= i_l) and (P_EXE_MODE = '0') and (P_MMU_MR = '1')) -- comparison func
+  i_addr <= (P_MMU_ADDR + i_b) when (P_PR = '1') and (P_MMU_MR = '1')            -- 加算機能
+            else P_MMU_ADDR ;                                              
+  i_intr <= '1' when ((P_MMU_ADDR >= i_l) and (P_PR = '0') and (P_MMU_MR = '1')) -- 比較機能
             else '0' ;
   i_mr <= P_MMU_MR and (not i_intr);
   P_ADDR <= i_addr;
