d5f780c937c6d1b3bfd5befd7d37c1af55cdd655
tctsigemura <sigemura@tokuyama.ac.jp>
2019-01-13 12:07:50 +0900

    テストプログラムを現状に合わせた

--- TaC/Test/8queen/crt0.s
+++ TaC/Test/8queen/crt0.s
@@ -1,8 +1,8 @@
 ;
-; TaC Kernel Source Code
+; TaC Test Source Code
 ;    Tokuyama kousen Educational Computer 16 bit Version
 ;
-; Copyright (C) 2009-2010 by
+; Copyright (C) 2009-2013 by
 ;                      Dept. of Computer Science and Electronic Engineering,
 ;                      Tokuyama College of Technology, JAPAN
 ;
@@ -17,169 +17,114 @@
 ; る損害に関しても，その責任を負わない．
 ;
 ;
-; crt0.s : ブートプログラム、カーネル兼用の C-- スタートアップ
-; 
-; 2010.07.20         : Subversion の管理に入る
-; 2009.11.05         : Boot 用と Kernel 用をマージしなおす
-; 2009.05.13  v0.1.1 : v0.1.0 から ei(), di() をバックポート
+; crt0.s : テストプログラムのアセンブラ部分
+;
+; 2013.01.07         : TeC-CPU V2 対応
+; 2012.01.11         : 初期バージョン
 ;
 ; $Id$
 ;
-;
-start				; IPL からここにジャンプしてくる
-	st	sp,.sp		; SP の値を保存しておく
-	call	___cstart
+
+start	ld	sp,#0xe000	; 主記憶 58kb
 	call	_main
-	jmp	_ipl
+	halt			; リスタート
 
-.sp	ds	1		; SP の保存領域
+; 1us 待つ
+; 1us  = 20ns * 50 = 50ステート
+; call = 6ステート、残り = 44 ステート
+_wait1u	ld	g0,g0		; 4ステート、残り = 40ステート
+	ld	g0,g0		; 4ステート、残り = 36ステート
+	ld	g0,g0		; 4ステート、残り = 32ステート
+	ld	g0,g0		; 4ステート、残り = 28ステート
+	ld	g0,g0		; 4ステート、残り = 24ステート
+	ld	g0,g0		; 4ステート、残り = 20ステート
+	ld	g0,g0		; 4ステート、残り = 16ステート
+	ld	g0,g0		; 4ステート、残り = 12ステート
+	no			; 3ステート、残り =  9ステート
+	no			; 3ステート、残り =  6ステート
+	ret			; 6ステート、残り =  0ステート
+	
+; 10us 待つ
+_wait10u
+	call	_wait1u
+	call	_wait1u
+	call	_wait1u
+	call	_wait1u
+	call	_wait1u
+	call	_wait1u
+	call	_wait1u
+	call	_wait1u
+	call	_wait1u		; 9us
+	ld	g0,g0		; 4ステート、残り = 46ステート
+	ld	g0,g0		; 4ステート、残り = 42ステート
+	ld	g0,g0		; 4ステート、残り = 38ステート
+	ld	g0,g0		; 4ステート、残り = 34ステート
+	ld	g0,g0		; 4ステート、残り = 30ステート
+	ld	g0,g0		; 4ステート、残り = 26ステート
+	ld	g0,g0		; 4ステート、残り = 22ステート
+	ld	g0,g0		; 4ステート、残り = 18ステート
+	no			; 3ステート、残り = 15ステート
+	no			; 3ステート、残り = 12ステート
+	no			; 3ステート、残り =  9ステート
+	no			; 3ステート、残り =  6ステート
+	ret			; 6ステート、残り =  0ステート
 
-_ei				; 割込み許可
-	ei
+; 100us 待つ
+_wait100u
+	call	_wait10u
+	call	_wait10u
+	call	_wait10u
+	call	_wait10u
+	call	_wait10u
+	call	_wait10u
+	call	_wait10u
+	call	_wait10u
+	call	_wait10u
+	call	_wait10u
 	ret
 
-_di				; 割込み禁止
-	di
+; 1ms 待つ
+; (88ステート(1.8us)超過)
+_wait1m
+	call	_wait100u
+	call	_wait100u
+	call	_wait100u
+	call	_wait100u
+	call	_wait100u
+	call	_wait100u
+	call	_wait100u
+	call	_wait100u
+	call	_wait100u
+	call	_wait100u
 	ret
 
-__exit
-_ipl	jmp	0xf800		; ROMモニタに戻る
-
-_cls	jmp	0xf802		; 画面クリア
-
-_putch
-	jmp	0xf804		; １文字表示
-
-_putstr	jmp	0xf806		; 文字列表示
-
-__puthex
-	jmp	0xf808		; １６進数表示(cmm 版を優先)
-
-_readSct
-	jmp	0xf80a		; １セクタ読み込み
-
-__jump  ld      g1,1,sp         ; _jump(int addr)
-	ld	sp,.sp		; 最初に保存した値に戻す
-        jmp     0,g1            ; 任意のアドレスにジャンプする
-
-_in				; 指定ポートから入力
-	push    g1		; int in(int port)
-        ld      g1,2,sp
-        in      g0,g1
-        pop     g1
+;; ワードを I/O ポートから入力する
+_in                             ; int in(int p);
+        ld      g1,2,sp         ; ポートアドレス
+        in      g0,g1           ; I/O ポートから入力する
         ret
 
-_out				; 指定ポートに出力
-	push    g0		; void out(int port, int val)
-        push    g1
-        ld      g0,3,sp
-        ld      g1,4,sp
-        out     g1,g0
-        pop     g1
-        pop     g0
+;; ワードを I/O ポートへ出力する
+_out                            ; void out(int p, int v);
+        ld      g0,2,sp         ; ポートアドレス
+        ld      g1,4,sp         ; 出力データ
+        out     g1,g0           ; I/O ポートへ出力する
         ret
 
-___AtoI				; アドレスから整数へ変換
-___ItoA				; 整数からアドレスへ変換
-	ld	g0,1,sp
+__AtoI				; アドレスから整数へ変換
+__ItoA				; 整数からアドレスへ変換
+	ld	g0,2,sp
 	ret
 
-___end  ld      g0,#__end       ; BSSセグメントの最後のアドレス
-        ret
+_jump	ld	g1,2,sp		; jump(addr)
+	ld	sp,#0xe000      ; RAM の最後
+	jmp	0,g1		; 任意のアドレスにジャンプする
 
-___memsize
-	ld	g0,sp	        ; スタックに 2KW を割り当てて
-        sub     g0,#2048        ; 残りをヒープとして使用する
-        sub     g0,#__end       ; ヒープのサイズを求める
-        ret
+_halt	halt			; halt()
+	jmp	_halt
 
-___AddrAdd                      ; アドレス演算
-        ld      g0,1,sp
-        add     g0,2,sp
-        ret
-
-___ucmp                         ; 符号無し比較
-___acmp                         ; アドレス比較
-        ld      g0,1,sp
-        cmp     g0,2,sp
-        ld      g0,#1
-        jhi     .L1
-        ld      g0,#0
-        jz      .L1
-        ld      g0,#-1
-.L1
-        ret
-
-___args                         ; 呼び出したC--関数の第2引数のアドレスを返す
-        ld      g0,fp
-        add     g0,#3
-        ret
-
-__add32				; 32ビット加算ルーチン
-	push	g1		; int[] _add32(int[] dst, int[] src)
-	push	g2
-	ld      g1,3,sp		; ディスティネーション(アドレス)
-	ld      g2,4,sp		; ソース(アドレス)
-	ld	g0,1,g1		; ディスティネーション下位ワード
-        add     g0,1,g2		; ソース下位ワード
-	st	g0,1,g1		; ディスティネーション下位ワード
-	ld	g0,0,g1		; ディスティネーション上位ワード
-	jnc	.L2
-	add	g0,#1		; キャリーがあった場合は +1 する
-.L2
-	add	g0,0,g2		; ソース上位ワード
-	st	g0,0,g1		; ディスティネーション上位ワード
-	ld	g0,g1		; ディスティネーションを返す
-	pop	g2
-	pop	g1
-	ret
-
-__sub32				; 32ビット減算ルーチン
-	push	g1		; int[] _sub32(int[] dst, int[] src)
-	push	g2
-	ld      g1,3,sp		; ディスティネーション(アドレス)
-	ld      g2,4,sp		; ソース(アドレス)
-	ld	g0,1,g1		; ディスティネーション下位ワード
-        sub     g0,1,g2		; ソース下位ワード
-	st	g0,1,g1		; ディスティネーション下位ワード
-	ld	g0,0,g1		; ディスティネーション上位ワード
-	jnc	.L3
-	sub	g0,#1		; ボローがあった場合は -1 する
-.L3
-	sub	g0,0,g2		; ソース上位ワード
-	st	g0,0,g1		; ディスティネーション上位ワード
-	ld	g0,g1		; ディスティネーションを返す
-	pop	g2
-	pop	g1
-	ret
-
-__mul32				; 32ビットかけ算ルーチン
-	push	g1		; int[] _mul32(int[] dst, int src)
-	push	g2		; (dst[0],dst[1]) <- dst[1] * src
-	push	g3
-	ld      g1,4,sp		; ディスティネーション(アドレス)
-	ld	g2,1,g1		; ディスティネーション下位ワード
-	mul	g2,5,sp		; ソース
-	st	g3,0,g1		; ディスティネーション上位ワード
-	st	g2,1,g1		; ディスティネーション下位ワード
-	ld	g0,g1		; ディスティネーションを返す
-	pop	g3
-	pop	g2
-	pop	g1
-	ret
-
-__div32				; 32ビット割算ルーチン
-	push	g1		; int[] _div32(int[] dst, int src)
-	push	g2		; dst[0] <- dst / src
-	push	g3		; dst[1] <- dst % src
-	ld      g1,4,sp		; ディスティネーション(アドレス)
-	ld	g2,1,g1		; ディスティネーション下位ワード
-	ld	g3,0,g1		; ディスティネーション上位ワード
-	div	g2,5,sp		; ソース
-	st	g3,1,g1		; ディスティネーション下位ワード(余)
-	st	g2,0,g1		; ディスティネーション上位ワード(商)
-	ld	g0,g1		; ディスティネーションを返す
-	pop	g3
-	pop	g2
-	pop	g1
-	ret
+_setPri                         ; プロセッサの割り込みレベルを変更
+        ld      g0,2,sp         ; 引数の値を G0 に取り出す
+        push    g0              ; 新しい状態をスタックに積み
+        ld      g0,flag         ; 古いフラグの値を返す準備をする
+        reti                    ; reti は flag と PC を同時に pop する
