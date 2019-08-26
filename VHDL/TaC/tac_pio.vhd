8c5e1fbc413c9846faadc566371ea049d70c5342
tctsigemura <sigemura@tokuyama.ac.jp>
2019-08-26 18:00:48 +0900

    tac_pio.vhd にSPIを追加

--- VHDL/TaC/tac_pio.vhd
+++ VHDL/TaC/tac_pio.vhd
@@ -21,20 +21,18 @@
 --
 -- TaC/tac_pio.vhd : TaC PIO
 --
--- 2019.07.30           : P_IOR に関する警告を消す
--- 2018.12.09           : PIOの出力を最大 12 ビット化
--- 2018.07.13           : モードを3ビットに変更
--- 2012.01.10           : 新規作成
+-- 2019.08.22 : SPI 機能を追加
+-- 2019.08.19 : ADC_REF の初期値を 0x00 から 0x80 に変更（適切な閾値）
+-- 2019.07.30 : P_IOR に関する警告を消す
+-- 2018.12.09 : PIOの出力を最大 12 ビット化
+-- 2018.07.13 : モードを3ビットに変更
+-- 2012.01.10 : 新規作成
 --
--- $Id
 --
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
---use IEEE.NUMERIC_STD.ALL;
---library UNISIM;
---use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.ALL;
+use ieee.std_logic_unsigned.ALL;
 
 entity TAC_PIO is
     Port ( P_CLK      : in  std_logic;
@@ -43,7 +41,7 @@ entity TAC_PIO is
 --         P_IOR      : in  std_logic;
            P_IOW      : in  std_logic;
            P_INT      : out  std_logic;
-           P_ADDR     : in  std_logic_vector (1 downto 0);
+           P_ADDR     : in  std_logic_vector (2 downto 0);
            P_DIN      : in  std_logic_vector (7 downto 0);
            P_DOUT     : out  std_logic_vector (7 downto 0);
 
@@ -60,33 +58,115 @@ architecture Behavioral of TAC_PIO is
   signal i_ext_mode   : std_logic;
   signal i_adc        : std_logic_vector(7 downto 0);
 
+  signal spi_cnt8     : std_logic_vector(7 downto 0);  -- 分周用カウンタ
+  signal spi_reg8     : std_logic_vector(7 downto 0);  -- コンペア用レジスタ
+  signal spi_match    : std_logic;                     -- コンペアマッチ
+  signal spi_sreg     : std_logic_vector(7 downto 0);  -- シフトレジスタ
+  signal spi_cnt3     : std_logic_vector(2 downto 0);  -- 8ビットカウント用
+  signal spi_run      : std_logic;                     -- 動作中
+  signal spi_ld       : std_logic;                     -- sreg の LD 信号
+  signal spi_sclk     : std_logic;                     -- クロック出力
+  signal spi_so       : std_logic;                     -- シリアル出力
+
 begin
+  -- PIO 関係
   process(P_RESET, P_CLK)
     begin
       if (P_RESET='0') then
         i_ext_mode <= '0';
         i_ext_out <= "000000000000";
-        i_adc <= "00000000";
+        i_adc <= "10000000";
       elsif (P_CLK'event and P_CLK='1') then
         if (P_EN='1' and P_IOW='1') then
-          if (P_ADDR(1)='0') then
-            if (P_ADDR(0)='0') then
-              i_ext_out(7 downto 0) <= P_DIN;
+          if (P_ADDR(2)='1') then
+            if (P_ADDR(1)='0') then
+              if (P_ADDR(0)='0') then                    -- 18h
+                i_ext_out(7 downto 0) <= P_DIN;
+              else
+                i_adc <= P_DIN;                          -- 1Ah
+              end if;
             else
-              i_adc <= P_DIN;
+              i_ext_mode <= P_DIN(7);                    -- 1Ch, 1Eh
+              i_ext_out(11 downto 8) <= P_DIN(3 downto 0);
             end if;
-          else
-            i_ext_mode <= P_DIN(7);
-            i_ext_out(11 downto 8) <= P_DIN(3 downto 0);
           end if;
         end if;
       end if;
     end process;
-      
-  P_DOUT <= P_EXT_IN when (P_ADDR(0)='0') else "00000" & P_MODE;
+
+  -- SPI 関係
+  spi_match <= '1' when spi_cnt8=spi_reg8 else '0';
+  process(P_RESET, P_CLK)
+    begin
+      if (P_RESET='0') then
+        spi_cnt8 <= "00000000";
+      elsif (P_CLK'event and P_CLK='1') then
+        if (spi_run='0' or spi_match='1') then
+          spi_cnt8 <= "00000000";
+        else
+          spi_cnt8 <= spi_cnt8 + 1;
+        end if;
+      end if;
+    end process;
+    
+  process(P_RESET, P_CLK)
+    begin
+      if (P_RESET='0') then
+        spi_sclk <= '0';
+      elsif (P_CLK'event and P_CLK='1') then
+        if (spi_ld='1') then
+          spi_sclk <= '0';
+        elsif (spi_run='1' and spi_match='1') then
+          spi_sclk <= not spi_sclk;
+        end if;
+      end if;
+    end process;
+
+  spi_ld <= P_EN and P_IOW when P_ADDR="000" else  '0';          -- 20h write
+  spi_so <= spi_sreg(7) and spi_run;
+  process(P_RESET, P_CLK)
+    begin
+      if (P_RESET='0') then
+        spi_run <= '0';
+        spi_sreg <= "00000000";
+        spi_cnt3 <= "000";
+      elsif (P_CLK'event and P_CLK='1') then
+        if (spi_ld='1') then
+          spi_run <= '1';
+          spi_sreg <= P_DIN;
+          spi_cnt3 <= "111";
+        elsif (spi_match='1' and spi_sclk='1') then
+          if (spi_cnt3="000") then
+            spi_run <= '0';
+          else
+            spi_cnt3 <= spi_cnt3 - 1;
+          end if;
+          spi_sreg(7 downto 1) <= spi_sreg(6 downto 0);
+          spi_sreg(0) <= P_EXT_IN(6);
+        end if;
+      end if;
+    end process;
+
+  process(P_RESET, P_CLK)
+    begin
+      if (P_RESET='0') then
+        spi_reg8 <= "00000000";
+      elsif (P_CLK'event and P_CLK='1') then
+        if (P_EN='1' and P_IOW='1' and P_ADDR="001") then        -- 22h write
+          spi_reg8 <= P_DIN;
+        end if;
+      end if;
+    end process;
+
+  P_DOUT <= (P_EXT_IN)            when (P_ADDR(2 downto 0)="100") else -- 18h
+            ("00000" & P_MODE)    when (P_ADDR(2 downto 0)="111") else -- 1Eh
+            (spi_sreg)            when (P_ADDR(2 downto 0)="000") else -- 20h
+            ("0000000" & spi_run);                                     -- 22h
+    
   P_ADC_REF <= i_adc;
-  P_EXT_OUT <= i_ext_out;
+  P_EXT_OUT(11 downto 2) <= i_ext_out(11 downto 2);
+  P_EXT_OUT(1) <= i_ext_out(1) xor spi_sclk;
+  P_EXT_OUT(0) <= i_ext_out(0) xor spi_so;
   P_EXT_MODE <= i_ext_mode;
   P_INT <= '0';
 end Behavioral;
-
