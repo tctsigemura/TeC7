9eb8cf14474c98f2c0d2982a7756c3af391ad2e5
tct-i15fujimura1s <i15fujimura1s.i15.tct@gmail.com>
2021-06-21 14:17:22 +0900

    Update tac_cpu_alu.vhd

--- VHDL/TaC/tac_cpu_alu.vhd
+++ VHDL/TaC/tac_cpu_alu.vhd
@@ -68,49 +68,45 @@ begin
     P_BUSY <= I_BUSY;
     P_OUT  <= I_OUT(15 downto 0);
 
-    I_OUT  <= '0' & P_A                 when P_OP1 = "00001" else   -- LD
+    I_OUT  <= '0' & P_B                 when P_OP1 = "00001" else   -- LD
               ('0' & P_A) + P_B         when P_OP1 = "00011" else   -- ADD
-              ('0' & P_A) - P_B         when P_OP1 = "00100" or     -- SUB
-                                             P_OP1 = "00101" else   -- CMP
+              ('0' & P_A) - P_B         when P_OP1(4 downto 1) = "0010"-- SUB, CMP
               '0' & (P_A and P_B)       when P_OP1 = "00110" else   -- AND
               '0' & (P_A or P_B)        when P_OP1 = "00111" else   -- OR
               '0' & (P_A xor P_B)       when P_OP1 = "01000" else   -- XOR
               ('0' & P_A) + (P_B & '0') when P_OP1 = "01001" else   -- ADDS
               (P_A * P_B)(16 downto 0)  when P_OP1 = "01010" else   -- MUL
-              P_A(15) & I_SHL           when P_OP1 = "10000" or     -- SHLA
-                                             P_OP1 = "10001" else   -- SHLL
-              P_A(15) & I_SHR           when P_OP1 = "10010" or     -- SHRA
-                                             P_OP1 = "10011" else   -- SHRL
+              P_A(15) & I_SHL           when P_OP1(4 downto 1) = "1000"-- SHLA, SHLL
+              P_A(0) & I_SHR            when P_OP1(4 downto 1) = "1001"-- SHRA, SHRL
               -- XXX: 本当にINのときにP_Bか確認する（OUT？）
-              P_B                       when P_OP1 = "10110" or     -- IN
+              '0' & P_B                 when P_OP1 = "10110" or     -- IN
                                              P_OP1 = "11000" else   -- PUSH
               (others => '0');
     
     -- 左シフト
     I_S1    <= P_A(14 downto 0) & "0" when P_B(0) = '1' else P_A;
-    I_S3    <= P_A(13 downto 0) & "00" when P_B(1) = '1' else I_S1;
-    I_S7    <= P_A(11 downto 0) & "0000" when P_B(2) = '1' else I_S3;
-    I_SHL   <= P_A(7 downto 0) & "00000000" when P_B(3) = '1' else I_S3;
+    I_S3    <= I_S1(13 downto 0) & "00" when P_B(1) = '1' else I_S1;
+    I_S7    <= I_S3(11 downto 0) & "0000" when P_B(2) = '1' else I_S3;q
+    I_SHL   <= I_S7(7 downto 0) & "00000000" when P_B(3) = '1' else I_S3;
 
     -- 右シフト
-    I_SIGH  <= P_A(15) when P_OP1 = "10010" else '0';
-    I_E8    <= (15 downto 8 => I_SIGH) & P_A(15 downto 8);
+    I_SIGN  <= P_A(15) when P_OP1 = "10010" else '0';
+    I_E8    <= (15 downto 8 => I_SIGN) & P_A(15 downto 8);
     I_S8    <= I_E8 when P_B(3) = '1' else P_A;
-    I_E12   <= (15 downto 12 => I_SIGH) & I_S8(15 downto 4);
-    I_S12   <= I_E4 when P_B(2) = '1' else I_S8;
-    I_E14   <= (15 downto 14 => I_SIGH) & I_S12(15 downto 2);
+    I_E12   <= (15 downto 12 => I_SIGN) & I_S8(15 downto 4);
+    I_S12   <= I_E12 when P_B(2) = '1' else I_S8;
+    I_E14   <= (15 downto 14 => I_SIGN) & I_S12(15 downto 2);
     I_S14   <= I_E14 when P_B(1) = '1' else I_S12;
-    I_E15   <= I_SIGH & I_S14(15 downto 1);
+    I_E15   <= I_SIGN & I_S14(15 downto 1);
     I_SHR   <= I_E15 when P_B(0) = '0' else I_S14;
 
     process (P_CLK, P_RESET)
     begin
-        I_FLAG <= '0';
         if (P_RESET = '1') then
             I_BUSY <= '0';
         elsif (P_CLK 'event and P_CLK = '1') then
-            if (P_START = '1') then
-                P_BUSY <= P_OP1 = "01011" or P_OP1 = "01100";
+            if (P_START = '1' and (P_OP1 = "01011" or P_OP1 = "01100")) then
+                P_BUSY <= '1';
             end if;
         end if;
     end process;
