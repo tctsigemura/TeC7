1bb8e6b8e0fde056a6d6f908f73d30c43035d283
makino naomiti <i15makino@Virtual-PC034.pc.tokuyama.ac.jp>
2021-07-20 14:13:46 +0900

    コメントを英字に書き換え，subtype宣言削除．配列宣言でVHDLバージョンエラー．

--- VHDL/TaC/tac_mmu.vhd
+++ VHDL/TaC/tac_mmu.vhd
@@ -6,24 +6,24 @@
 --                      Dept. of Computer Science and Electronic Engineering,
 --                      Tokuyama College of Technology, JAPAN
 --
---   上記著作権者は，Free Software Foundation によって公開されている GNU 一般公
--- 衆利用許諾契約書バージョン２に記述されている条件を満たす場合に限り，本ソース
--- コード(本ソースコードを改変したものを含む．以下同様)を使用・複製・改変・再配
--- 布することを無償で許諾する．
+--   Free Software Foundation  GNU 
+-- 
+-- ()
+-- 
 --
---   本ソースコードは＊全くの無保証＊で提供されるものである。上記著作権者および
--- 関連機関・個人は本ソースコードに関して，その適用可能性も含めて，いかなる保証
--- も行わない．また，本ソースコードの利用により直接的または間接的に生じたいかな
--- る損害に関しても，その責任を負わない．
+--   
+-- 
+-- 
+-- 
 --
 --
 
 --
 -- TaC/tac_mmu.vhd : TaC Memory Management Unit Source Code
 --
--- 2019.12.19           : CPU停止時（コンソール動作時）はアドレス変換禁止
--- 2019.07.30           : アドレスエラー追加
--- 2019.01.22           : 新しく追加
+-- 2019.12.19           : CPU
+-- 2019.07.30           : 
+-- 2019.01.22           : 
 --
 
 library IEEE;
@@ -44,32 +44,30 @@ entity TAC_MMU is
          P_MR       : out std_logic;                     -- Memory Request
          P_ADDR     : out std_logic_vector(15 downto 0); -- Physical address
          P_MMU_ADDR : in  std_logic_vector(15 downto 0); -- Virtual address
-         P_DIN      : in  std_logic_vector(15 downto 0); --TLBのエントリ
-         P_DOUT     : out std_logic_vector(15 downto 0);         
-         P_IOR      : out std_logic;                     --どのページでミスしたか知らせる  
+         P_DIN      : in  std_logic_vector(15 downto 0); 
+         P_DOUT     : out std_logic_vector(15 downto 0); 
+         P_IOR      : out std_logic;                      
        );
 end TAC_MMU;
-
---プロセス切替時、ページテーブルを読み込めるようにする
 
 architecture Behavioral of TAC_MMU is
 signal i_en  : std_logic;                                -- Enable resister
 --signal i_b   : std_logic_vector(15 downto 0);            -- Base  register
 --signal i_l   : std_logic_vector(15 downto 0);            -- Limit register
-signal i_act : std_logic;                                -- Activate MMUが動くか
+signal i_act : std_logic;                                -- Activate MMU
 signal i_vio : std_logic;                                -- Memory Violation
 --signal i_adr : std_logic;                                -- Bad Address
-
-subtype TLB_field is std_logic_vector(23 downto 0);     --page + ctrl + frame
-type TLB_array is array (7 downto 0) of TLB_field;      --24bit * 8 array
+ 
+//error not supported
+type TLB_array is array(7 downto 0) of std_logic_vector(23 downto 0);   --array of 24bit * 8 
 
 signal TLB : TLB_array;                                 --TLB
-signal tar_field : TLB_field;                           --検索したtlb field
-signal key_page : std_logic_vector(7 downto 0);         --キーとなるページ番号  
-signal tar_frame : std_logic_vector(7 downto 0);        --frame number
-signal entry : std_logic_vector(15 downto 0);           --c + frame
-signal index : std_logic_vector(10 downto o);           --n + page
-signal flag : std_logic;
+signal key_page : std_logic_vector(7 downto 0);         --page num of key
+signal tar_field : std_logic_vector(23 downto 0);       --TLB field gotten with key
+signal tar_frame : std_logic_vector(7 downto 0);        --frame num gotten with key
+signal entry : std_logic_vector(15 downto 0);           --ctrl + frame num
+signal index : std_logic_vector(10 downto 0);           --page num index + page num
+signal flag : std_logic;                                --update flag
 
 begin 
 
@@ -77,11 +75,12 @@ begin
   offset <= P_ADDR(7 downto 0);
   tar_frame <= tar_field(7 downto 0);
 
-  --CAM for loopが使える？
+  --CAM  
+  --can use for loop?
   process(P_CLK)
   begin 
     if (P_CLK'event and P_CLK='1') then
-    --if(なにかしらの条件)
+    --if()
         if (key_page=TLB[0])
           tar_field <= TLB[0];
         elsif (key_page=TLB[1])
@@ -104,14 +103,14 @@ begin
     end if
   end process
   
-  --エントリの入れ替え
+  --ready to update entry
   process(P_CLK)
   begin 
     if(P_RESET]=0) then
       --TLB clear?
       i_en <= 0;
     elsif (P_CLK'event and P_CLK='1') then
-      if(P_EN='1' and P_IOW='1') then //書き込みモード時はP_MMU_ADDRとP_DIN
+      if(P_EN='1' and P_IOW='1') then 
         if(P_MMU_ADDR(2)='0') then
           i_en <= '1';
         elsif(P_MMU_ADDR(1)='0') then    
@@ -124,11 +123,12 @@ begin
     end if;
   end process;
 
-  --for loopかcaseが使える？
+  --update entry 
+  --can use for loop or case?
   process(P_CLK)
   begin
     if(P_CLK'event and P_CLK='1') then
-      if(f='1') then
+      if(flag='1') then
         if(index(10 downto 8)="000") then
           TLB[0](23 downto 16) <= index(7 downto 0);
           TLB[0](15 downto 0) <= entry;
@@ -155,7 +155,7 @@ begin
           TLB[7](15 downto 0) <= entry;
         end if;
       end if;
-      f <= '0';
+      flag <= '0';
     end if;
   end process;
 
