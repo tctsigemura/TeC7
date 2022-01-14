77a4e97e763cda2fc9d6e075de4377bbad440d90
tct-i15fujimura1s <fujimura1s.i15.tct@gmail.com>
2022-01-14 13:33:42 +0900

    SELECT_D=5のときの挙動を変更

--- VHDL/TaC/tac_cpu_sequencer.vhd
+++ VHDL/TaC/tac_cpu_sequencer.vhd
@@ -268,7 +268,8 @@ begin
 
   -- DOUT
   P_SELECT_D <= "010" when I_NEXT=S_CALL else                      -- PC+4
-                "101" when P_OP2="111" and P_ADDR0='0' else       -- GR[Rd]>>>8
+                "101" when (I_NEXT=S_ST1 or I_NEXT=S_ST2) and 
+                          P_OP2="111" and P_ADDR0='0' else       -- GR[Rd]>>>8
                 "100" when ((I_STATE=S_DEC1 or I_STATE=S_DEC2) and -- GR[Rd]
                             I_NEXT=S_FETCH) or
                            I_NEXT=S_ST1 or I_NEXT=S_ST2 or
