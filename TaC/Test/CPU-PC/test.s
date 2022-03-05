b8c909727a42c4b46531d61dd4ff5b31bec81c47
重村(SHIGEMURA)　哲至(TETSUJI) <sigemura@tokuyama.kosen-ac.jp>
2022-03-05 17:13:39 +0900

    インデクスモードのJMP, CALLテストを追加 	modified:   TaC/Test/CPU-PC/test.s

--- TaC/Test/CPU-PC/test.s
+++ TaC/Test/CPU-PC/test.s
@@ -3,15 +3,21 @@
 ;;;
 start	halt
 	ld	sp,#0x0100	; sp=0100
-	call	L1		; sp=00fe, [00fe]=000a
-	no			; sp=0100
-	jmp	L2		; 
-	no
-	no
+	call	L1		; sp=00fe, [00fe]=000a, pc=0024
+	no			;
+	ld	fp,#2		; fp=0002
+	call	L1,fp		; sp=00fe, [00fe]=0012, pc=0026
+	no			;
+	jmp	L2		; pc=001a
+	no			; ここは実行されない
 L2	
-	halt
+	jmp	L3,fp		; pc=0020
+L3
+	no			; ここは実行されない
+	no			;
+	halt			; ここで停止
 
 L1
-	no
+	no			;
 	RET			; sp=0100
 
